6ed709bb48650bec3febb7141097cfe30684127f
brunolobell <44982789+brunolobell@users.noreply.github.com>
2019-10-30 11:21:16 -0300

    Add files via upload

---
+++ Bruno(124846)_Johann(116532).cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#define MAX 10
+
+int Insert_Node(int Matriz[MAX][MAX]){
+    
+}
+
+void Insert_Edge(int Node1, int Node2, int ** Matriz){
+    
+}
+
+int Remove_Node(int Node_Remove, int ** Matriz){
+
+}
+
+void Remove_Edge(int Node_Remove, int ** Matriz){
+
+}
+
+void Show_Graph(int Size, int ** Matriz){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int Matriz[MAX][MAX];
+    int case_,Count = 0;
+    for(;;){
+        switch (case_)
+        {
+            case 1:
+                
+                break;
+
+            case 2:
+                
+                break;
+
+            case 3:
+                
+                break;
+            
+            case 4:
+                
+                break;
+            
+            case 5:
+                
+                break;
+
+            default:
+                break;
+        }
+    Count = Insert_Node(Matriz);
+
+    return 0;
+}
